1da38cd298b0889e0e1d2f1d0e6f16c54744bbfa
xAnGGx <angenispb@mail.ru>
2021-09-08 17:03:06 +0300

    Added user list with hardcoded users

--- app/src/main/java/com/georgiyangeni/firstandroidapp/MainActivity.kt
+++ app/src/main/java/com/georgiyangeni/firstandroidapp/MainActivity.kt
@@ -6,24 +6,87 @@ import android.view.View
 import android.widget.Button
 import android.content.Intent
 import android.net.Uri
+import android.os.PersistableBundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
-// class MainActivity : AppCompatActivity(R.layout.activity_main)
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.usersRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val adapter = UserAdapter()
+        recyclerView.adapter = adapter
+        adapter.userList = loadUsers()
+        adapter.notifyDataSetChanged()  // will come back to this later
     }
 
-    fun onFirstButtonClick(view: View?) {
-        if (view != null) {
-            if (view.id == R.id.first_button) {
-                val uri: Uri =
-                    Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    private fun loadUsers(): List<User> {
+        return listOf(
+            User(
+                avatarUrl = "",
+                userName = "User name 1",
+                groupName = "A"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 2",
+                groupName = "AB"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 3",
+                groupName = "CA"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 4",
+                groupName = "DB"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 5",
+                groupName = "BA"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 6",
+                groupName = "C"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 7",
+                groupName = "D"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 8",
+                groupName = "AE"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 9",
+                groupName = "Sausage school"
+            )
+        )
+}
 
-                val intent = Intent(Intent.ACTION_VIEW, uri)
-                startActivity(intent)
-            }
-        }
-    }
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+
+//    fun onFirstButtonClick(view: View?) {
+//        if (view != null) {
+//            if (view.id == R.id.first_button) {
+//                val uri: Uri =
+//                    Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+//
+//                val intent = Intent(Intent.ACTION_VIEW, uri)
+//                startActivity(intent)
+//            }
+//        }
+//    }
 }
---
+++ app/src/main/java/com/georgiyangeni/firstandroidapp/User.kt
@@ -0,0 +1,7 @@
+package com.georgiyangeni.firstandroidapp
+
+data /* unique methods */ class User(
+    val avatarUrl: String, // http/https link
+    val userName: String,
+    val groupName: String
+)
