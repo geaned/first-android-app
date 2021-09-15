3115ccca8100b0ee39748aa2e70056541f44dc6e
xAnGGx <angenispb@mail.ru>
2021-09-15 16:43:27 +0300

    Now getting users from the Internet (reqres.in)

--- app/src/main/java/com/georgiyangeni/firstandroidapp/MainActivity.kt
+++ app/src/main/java/com/georgiyangeni/firstandroidapp/MainActivity.kt
@@ -3,73 +3,117 @@ package com.georgiyangeni.firstandroidapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.content.Intent
-import android.net.Uri
-import android.os.PersistableBundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val recyclerView = findViewById<RecyclerView>(R.id.usersRecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        val adapter = UserAdapter()
-        recyclerView.adapter = adapter
-        adapter.userList = loadUsers()
-        adapter.notifyDataSetChanged()  // will come back to this later
+        val adapter = setupRecyclerView()
+
+        findViewById<View>(R.id.usersRecyclerView).isVisible = false
+        findViewById<View>(R.id.progressBar).isVisible = true
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                adapter.userList = loadUsers()
+                adapter.notifyDataSetChanged()  // will come back to this later
+                findViewById<View>(R.id.usersRecyclerView).isVisible = true
+                findViewById<View>(R.id.progressBar).isVisible = false
+            }
+        }
     }
 
-    private fun loadUsers(): List<User> {
-        return listOf(
-            User(
-                avatarUrl = "",
-                userName = "User name 1",
-                groupName = "A"
-            ),
-            User(
-                avatarUrl = "",
-                userName = "User name 2",
-                groupName = "AB"
-            ),
-            User(
-                avatarUrl = "",
-                userName = "User name 3",
-                groupName = "CA"
-            ),
-            User(
-                avatarUrl = "",
-                userName = "User name 4",
-                groupName = "DB"
-            ),
-            User(
-                avatarUrl = "",
-                userName = "User name 5",
-                groupName = "BA"
-            ),
-            User(
-                avatarUrl = "",
-                userName = "User name 6",
-                groupName = "C"
-            ),
-            User(
-                avatarUrl = "",
-                userName = "User name 7",
-                groupName = "D"
-            ),
-            User(
-                avatarUrl = "",
-                userName = "User name 8",
-                groupName = "AE"
-            ),
-            User(
-                avatarUrl = "",
-                userName = "User name 9",
-                groupName = "Sausage school"
-            )
-        )
+    private fun setupRecyclerView(): UserAdapter {
+        val recyclerView = findViewById<RecyclerView>(R.id.usersRecyclerView)
+        val adapter = UserAdapter()
+        recyclerView.adapter = adapter
+        return adapter
+    }
+
+    private suspend fun loadUsers(): List<User> {
+        return withContext(Dispatchers.IO) {
+            //Thread.sleep(3000)  // for progress bar testing
+            provideApi().getUsers().data
+        }
+    }
+
+    private fun provideApi(): Api {
+        return Retrofit.Builder()
+            .client(provideOkHttpClient())
+            .baseUrl("https://reqres.in/api/")
+            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
+            .build()
+            .create(Api::class.java)
+    }
+
+    private fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    private fun provideMoshi(): Moshi {
+        return Moshi.Builder().build()
+    }
+
+//    private fun loadUsers(): List<User> {
+//        return listOf(
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 1",
+//                groupName = "A"
+//            ),
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 2",
+//                groupName = "AB"
+//            ),
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 3",
+//                groupName = "CA"
+//            ),
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 4",
+//                groupName = "DB"
+//            ),
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 5",
+//                groupName = "BA"
+//            ),
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 6",
+//                groupName = "C"
+//            ),
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 7",
+//                groupName = "D"
+//            ),
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 8",
+//                groupName = "AE"
+//            ),
+//            User(
+//                avatarUrl = "",
+//                userName = "User name 9",
+//                groupName = "Sausage school"
+//            )
+//        )
 }
 
 //class MainActivity : AppCompatActivity() {
@@ -89,4 +133,4 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 //            }
 //        }
 //    }
-}
+//}
--- app/src/main/java/com/georgiyangeni/firstandroidapp/User.kt
+++ app/src/main/java/com/georgiyangeni/firstandroidapp/User.kt
@@ -1,7 +1,11 @@
 package com.georgiyangeni.firstandroidapp
 
-data /* unique methods */ class User(
-    val avatarUrl: String, // http/https link
-    val userName: String,
-    val groupName: String
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class User(
+    @Json(name = "avatar") val avatarUrl: String, // http/https link
+    @Json(name = "first_name") val userName: String,
+    @Json(name = "email") val groupName: String
 )
