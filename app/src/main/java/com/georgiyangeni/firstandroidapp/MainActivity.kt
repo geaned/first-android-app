cb8bdc251f7b0b9a250a6391ace77fbed3a29e81
xAnGGx <angenispb@mail.ru>
2021-09-22 15:30:48 +0300

    Fixed earlier commit

--- app/src/main/java/com/georgiyangeni/firstandroidapp/MainActivity.kt
+++ app/src/main/java/com/georgiyangeni/firstandroidapp/MainActivity.kt
@@ -2,35 +2,59 @@ package com.georgiyangeni.firstandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.moshi.Moshi
-import kotlinx.coroutines.Dispatchers
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.georgiyangeni.firstandroidapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    companion object {
+        val LOG_TAG = "MyLogTag"
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private val viewBinding by viewBinding(ActivityMainBinding::bind)
+
+    // why not?
+    // private var adapter: UserAdapter - and use adapter instead of casting in renderViewState
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val adapter = setupRecyclerView()
-
-        findViewById<View>(R.id.usersRecyclerView).isVisible = false
-        findViewById<View>(R.id.progressBar).isVisible = true
+        Log.d(LOG_TAG, "onCreate()")
+        setContentView(R.layout.activity_main)
+        setupRecyclerView()
 
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                adapter.userList = loadUsers()
-                adapter.notifyDataSetChanged()  // will come back to this later
-                findViewById<View>(R.id.usersRecyclerView).isVisible = true
-                findViewById<View>(R.id.progressBar).isVisible = false
+                viewModel.viewState.collect { viewState ->
+                    renderViewState(viewState)
+                }
+            }
+        }
+    }
+
+    private fun renderViewState(viewState: MainViewModel.ViewState) {
+        when (viewState) {
+            is MainViewModel.ViewState.Loading -> {
+                viewBinding.usersRecyclerView.isVisible = false
+                viewBinding.progressBar.isVisible = true
+            }
+            is MainViewModel.ViewState.Data -> {
+                viewBinding.usersRecyclerView.isVisible = true
+                (viewBinding.usersRecyclerView.adapter as UserAdapter).apply {
+                    userList = viewState.userList
+                    notifyDataSetChanged()
+                }
+                viewBinding.progressBar.isVisible = false
             }
         }
     }
@@ -41,96 +65,4 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         recyclerView.adapter = adapter
         return adapter
     }
-
-    private suspend fun loadUsers(): List<User> {
-        return withContext(Dispatchers.IO) {
-            //Thread.sleep(3000)  // for progress bar testing
-            provideApi().getUsers().data
-        }
-    }
-
-    private fun provideApi(): Api {
-        return Retrofit.Builder()
-            .client(provideOkHttpClient())
-            .baseUrl("https://reqres.in/api/")
-            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
-            .build()
-            .create(Api::class.java)
-    }
-
-    private fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder().build()
-    }
-
-    private fun provideMoshi(): Moshi {
-        return Moshi.Builder().build()
-    }
-
-//    private fun loadUsers(): List<User> {
-//        return listOf(
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 1",
-//                groupName = "A"
-//            ),
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 2",
-//                groupName = "AB"
-//            ),
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 3",
-//                groupName = "CA"
-//            ),
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 4",
-//                groupName = "DB"
-//            ),
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 5",
-//                groupName = "BA"
-//            ),
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 6",
-//                groupName = "C"
-//            ),
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 7",
-//                groupName = "D"
-//            ),
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 8",
-//                groupName = "AE"
-//            ),
-//            User(
-//                avatarUrl = "",
-//                userName = "User name 9",
-//                groupName = "Sausage school"
-//            )
-//        )
 }
-
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-
-//    fun onFirstButtonClick(view: View?) {
-//        if (view != null) {
-//            if (view.id == R.id.first_button) {
-//                val uri: Uri =
-//                    Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-//
-//                val intent = Intent(Intent.ACTION_VIEW, uri)
-//                startActivity(intent)
-//            }
-//        }
-//    }
-//}
