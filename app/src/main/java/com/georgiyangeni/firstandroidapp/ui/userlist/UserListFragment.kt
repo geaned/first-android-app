a00190be849caccf8ad6ba8b50b81abd90b29cc6
xAnGGx <angenispb@mail.ru>
2021-11-24 00:47:27 +0300

    Moved UserList-related functionality to a separate ViewModel

--- app/src/main/java/com/georgiyangeni/firstandroidapp/ui/userlist/UserListFragment.kt
+++ app/src/main/java/com/georgiyangeni/firstandroidapp/ui/userlist/UserListFragment.kt
@@ -22,13 +22,13 @@ import kotlinx.coroutines.launch
 
 class UserListFragment : BaseFragment(R.layout.fragment_userlist) {
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: UserListViewModel
 
     private val viewBinding by viewBinding(FragmentUserlistBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel = ViewModelProvider(this)[UserListViewModel::class.java]
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -37,21 +37,20 @@ class UserListFragment : BaseFragment(R.layout.fragment_userlist) {
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-//                viewModel.viewState.collect { viewState ->
-//                    renderViewState(viewState)
-//                }
-                viewModel.viewState.collect(::renderViewState)
+                viewModel.viewState.collect { viewState ->
+                    renderViewState(viewState)
+                }
             }
         }
     }
 
-    private fun renderViewState(viewState: MainViewModel.ViewState) {
+    private fun renderViewState(viewState: UserListViewModel.ViewState) {
         when (viewState) {
-            is MainViewModel.ViewState.Loading -> {
+            is UserListViewModel.ViewState.Loading -> {
                 viewBinding.usersRecyclerView.isVisible = false
                 viewBinding.progressBar.isVisible = true
             }
-            is MainViewModel.ViewState.Data -> {
+            is UserListViewModel.ViewState.Data -> {
                 viewBinding.usersRecyclerView.isVisible = true
                 (viewBinding.usersRecyclerView.adapter as UserAdapter).apply {
                     userList = viewState.userList
---
+++ app/src/main/java/com/georgiyangeni/firstandroidapp/ui/userlist/UserListViewModel.kt
@@ -0,0 +1,91 @@
+package com.georgiyangeni.firstandroidapp.ui.userlist
+
+import androidx.lifecycle.viewModelScope
+import com.georgiyangeni.firstandroidapp.BuildConfig
+import com.georgiyangeni.firstandroidapp.data.network.Api
+import com.georgiyangeni.firstandroidapp.data.network.MockApi
+import com.georgiyangeni.firstandroidapp.ui.base.BaseViewModel
+import com.georgiyangeni.firstandroidapp.entity.User
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import timber.log.Timber
+
+class UserListViewModel : BaseViewModel() {
+    sealed class ViewState {
+        object Loading : ViewState()
+        data class Data(val userList: List<User>) : ViewState()
+    }
+
+    private val _viewState = MutableStateFlow<ViewState>(ViewState.Loading)
+    val viewState: Flow<ViewState> get() = _viewState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _viewState.emit(ViewState.Loading)
+            val users = loadUsers()
+            _viewState.emit(ViewState.Data(users))
+        }
+    }
+
+    private suspend fun loadUsers(): List<User> {
+        return withContext(Dispatchers.IO) {
+            Timber.d("loadUsers()")
+            // for progress bar testing
+            // Thread.sleep(3000)
+            provideApi().getUsers().data
+        }
+    }
+
+    private fun provideApi(): Api =
+        if (BuildConfig.USE_MOCK_BACKEND_API) {
+            MockApi()
+        } else {
+            Retrofit.Builder()
+                .client(provideOkHttpClient())
+                .baseUrl("https://reqres.in/api/")
+                .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
+                .build()
+                .create(Api::class.java)
+        }
+
+//    private fun provideOkHttpClient(): OkHttpClient {
+//        return OkHttpClient.Builder().build()
+//    }
+
+    private fun provideOkHttpClient(/*authRepository: AuthRepository*/): OkHttpClient =
+        OkHttpClient
+            .Builder()
+//            .addInterceptor(AuthorizationInterceptor(AuthRepository(...)))
+            .apply {
+//                readTimeout(60, TimeUnit.SECONDS)
+//                connectTimeout(60, TimeUnit.SECONDS)
+//                addNetworkInterceptor(UserAgentInterceptor(userAgent))
+//                addNetworkInterceptor(AuthorizationInterceptor(authRepository))
+//                authenticator(FirstAndroidAppAuthenticator(authRepository))
+                if (BuildConfig.DEBUG) {
+                    addNetworkInterceptor(
+                        HttpLoggingInterceptor { message ->
+                            Timber.d(message)
+                        }.setLevel(HttpLoggingInterceptor.Level.BODY)
+                    )
+//                    sslSocketFactory(
+//                        clientCertificates.sslSocketFactory(),
+//                        clientCertificates.trustManager
+//                    )
+                }
+            }
+            .build()
+
+    private fun provideMoshi(): Moshi {
+        return Moshi.Builder().build()
+    }
+}
